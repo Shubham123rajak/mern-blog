3e729ca72673550095c02cd382ed0cc532d00fcf
Shubham123rajak <shuhamrajak258@gmail.com>
2024-06-22 17:11:28 +0530

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,33 +1,51 @@
 import React from 'react'
-import {FaSearch} from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+// import {FaMagento, FaMoon, FaSearch} from 'react-icons/fa';
+import { AiOutlineSearch} from 'react-icons/ai'
+import { Link,useLocation} from 'react-router-dom';
+import { Button, Navbar, TextInput } from 'flowbite-react';
+import { FaMoon } from 'react-icons/fa';
 
 export default function Header() {
+    const path = useLocation().pathname;
   return (
-    <header className='bg-slate-200 shadow-md'>
-        <div className='flex justify-between item-center max-w max-w-6xl mx-auto p-3'>
-            <Link to='/'>
-       <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-        <span className = 'text-slate-500'>Sahand</span>
-        <span className = 'text-slate-700'>Estate</span>
-       </h1>
-       </Link>
-       <form  className='bg-slate-100 p-3 rounded-lg flex items-center'>
-         <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-         <FaSearch className='tex-slate-600'/>
-       </form>
-       <ul className='flex gap-4'>
-        <Link to='/'>
-        <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+    <Navbar className='border-b-2'>
+        <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+       
+         <span className = 'px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Sahand's</span>
+         <span>Blog</span>
         </Link>
-        <Link to='/about'>
-        <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
-        </Link>
-        <Link to='/sign-in'>
-        <li className='hidden sm:inline text-slate-700 hover:underline'>Sign in</li>
-        </Link>
-       </ul>
+        <form>
+            <TextInput type='text' placeholder='search..' rightIcon={AiOutlineSearch} className='hidden lg:inline'/>
+        </form>
+        <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+            <AiOutlineSearch/>
+        </Button>
+        <div className='flex gap-2 md:order-2'>
+            <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+                <FaMoon />
+            </Button>
+            <Link to='/sign-in'>
+                <Button gradientDuoTone='purpleToBlue' outline>Sign In</Button>
+            </Link>
+            <Navbar.Toggle/>
         </div>
-    </header>
+            <Navbar.Collapse>
+                <Navbar.Link active={path === "/" } as={'div'}>
+                    <Link to='/'>
+                       Home
+                    </Link>
+                </Navbar.Link>
+                <Navbar.Link active={path === "/about"} as={'div'}>
+                    <Link to='/about'>
+                       About
+                    </Link>
+                </Navbar.Link>
+                <Navbar.Link active={path === "/projects"} as={'div'}>
+                    <Link to='/projects'>
+                       Project
+                    </Link>
+                </Navbar.Link>
+            </Navbar.Collapse>
+    </Navbar>
   )
 }
