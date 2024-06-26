2c58becf521d627ee23858c34470d390dfffefac
Shubham123rajak <shuhamrajak258@gmail.com>
2024-06-26 08:17:57 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
 import bcryptjs from 'bcryptjs';
 import { errorHendler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const Signup = async (req, res, next) => {
     // console.log(req.body);
@@ -20,6 +21,30 @@ export const Signup = async (req, res, next) => {
         res.json('Signup successful');
     } catch (error) {
         console.log("hello");
+        next(error);
+    }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password || email === '' || password === '') {
+        next(errorHendler(400, 'All field are required'));
+    }
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHendler(400, 'User not found'))
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHendler(400, 'invalid password'));
+        }
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true
+        }).json(validUser);
+    } catch (error) {
         next(error);
     }
 }
